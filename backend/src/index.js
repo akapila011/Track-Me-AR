4f81f59020506c3469bf3d64967f343808eb2dd5
Abhishek Kapila <akapila011@gmail.com>
2019-11-25 20:42:23 +0300

    Linked url to controller function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import {getBaseUrl, isDev} from "./util/config";
+import {userController} from "./controllers";
 
 
 const BASE_URL = getBaseUrl();
 
 const app = express();
 
+app.use(express.json());
+
 app.get(`/`, (req, res) => {res.send('Hello World!');});
+app.post(`/saveUser`, userController.saveUserController);
+
 
 
 if (isDev()){
